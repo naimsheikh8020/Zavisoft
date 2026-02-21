453ca8250efbd8433acf5d24e496bca3fc833ee1
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-02-21 09:19:51 +0600

    hero text done

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,8 +1,14 @@
+import { assets } from "../assets/assets"
 
 const Home = () => {
   return (
-    <div>
-        <h1 className="text-2xl text-black">Home</h1>
+    <div className="w-full">
+      {/* Hero Text Section */}
+        <h1 className="text-4xl  sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  font-black uppercase leading-none text-center mt-4 md:mt-8">
+          <span className="text-black">DO IT </span>
+          <span className="text-blue-500">RIGHT</span>
+        </h1>
+     
     </div>
   )
 }
